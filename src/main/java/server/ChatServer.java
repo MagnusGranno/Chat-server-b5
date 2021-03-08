c3f077c2e10ad3ee172b1568bd75eb3673d9d930
MagnusGranno <70953393+MagnusGranno@users.noreply.github.com>
2021-03-08 10:44:37 +0100

    Fixes

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -8,19 +8,16 @@ import java.net.UnknownHostException;
 public class ChatServer {
 
 
-    //Call server with arguments like this: 0.0.0.0 8088 logfile.log
+    //Call server with arguments like this: 8080
     public static void main(String[] args) throws UnknownHostException {
-        String ip ="localhost";
         int port = 8088;
-        String logFile = "log.txt";  //Do we need this
+
 
         try {
-            if (args.length == 3) {
-                ip = args[0];
-                port = Integer.parseInt(args[1]);
-                logFile = args[2];
-            }
-            else {
+            if (args.length == 1) {
+                port = Integer.parseInt(args[0]);
+
+            } else {
                 throw new IllegalArgumentException("Server not provided with the right arguments");
             }
         } catch (NumberFormatException ne) {
